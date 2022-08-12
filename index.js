23f5ef3bd1617623add97252b2e3efad678826d2
KuuLotus <kuulotusy@gmail.com>
2022-08-12 23:46:30 +0900

    [finish] 関数

--- index.js
+++ index.js
@@ -37,3 +37,30 @@ console.log(obj.test); // hogeがnullなのでdef.testが代入（hogeがundefin
 const param = { obj: "param" };
 const data = `${param.obj}を展開`;
 console.log(data);
+
+////// ====== 関数 ====== //////
+
+// ES5
+var testES5 = function (param) {
+  console.log(param);
+};
+testES5("ES5");
+
+// ES6はアロー関数でも記述できる
+const testES6 = (param) => {
+  console.log(param);
+};
+testES6("ES6");
+
+// ES6では関数にデフォルト引数を持たせられる
+const init = { obj: "obj" };
+const testES6Def = (arg = init) => {
+  console.log(arg);
+};
+testES6Def();
+
+// オブジェクト引数を展開済みで渡す
+const testES6Obj = ({ a, b }) => {
+  console.log(a, b);
+};
+testES6Obj({ a: "a", b: "b" });
